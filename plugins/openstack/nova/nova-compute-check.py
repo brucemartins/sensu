ce52e2a3399a4df34144bfce6cdbd1163e8bfe69
brumarti <brumarti@cisco.com>
2014-11-12 21:00:31 -0500

    initial commit of Nova check script

---
+++ plugins/openstack/nova/nova-compute-check.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+#
+# Created By Bruce Martins
+# email: bmartins At gmail Dot com
+# Nova Agent Status check, doesn't require the nova python client 
+# and can be run remotely
+#
+# loosely  based on neutron-agent-status.py by
+# Brian Clark <brian.clark@cloudapt.com>
+#
+# Released under the same terms as Sensu (the MIT license);
+# see LICENSE for details.
+#
+import sys
+import json
+import logging
+import argparse
+import requests
+
+STATE_OK = 0
+STATE_WARNING = 1
+STATE_CRITICAL = 2
+STATE_UNKNOWN = 3
+
+#logging.basicConfig(level=logging.INFO)
+
+parser = argparse.ArgumentParser(description='Check OpenStack Nova Compute status')
+parser.add_argument('--auth-url', metavar='URL', type=str, default='http://localhost',
+                    required=False,
+                    help='Keystone URL')
+parser.add_argument('--username', metavar='username', type=str, default='admin',
+                    required=False,
+                    help='username for authentication')
+parser.add_argument('--password', metavar='password', type=str, default='admin',
+                    required=False,
+                    help='password for authentication')
+parser.add_argument('--tenant', metavar='tenant', type=str, default='admin',
+                    required=False,
+                    help='tenant name for authentication')
+
+args = parser.parse_args()
+
+def getAuthToken():
+        header = {'content-type': 'application/json'}
+        payload = {"auth": {"tenantName": args.tenant,"passwordCredentials": {"username": args.username,"password": args.password}}}
+        request = requests.post(args.auth_url + ':5000/v2.0/tokens', data=json.dumps(payload), headers=header)
+        response = json.loads(request.text)
+        token = response.get("access").get("token").get("id")
+        endpoint = response.get("access").get("token").get("tenant").get("id")
+        return [token,endpoint] 
+
+    
+def getComputeState(token):
+    tenantID = token[1]
+    auth_token = token [0]
+    url = args.auth_url + ':8774/v2/' + tenantID + '/os-services'
+    header = {'content-type': 'application/json','X-Auth-Token': auth_token}
+    request = requests.get(url, data=None, headers=header)
+    response = json.loads(request.text)
+    arr = response['services']     
+    outer_dict = arr[0]
+    inner_dict_state = outer_dict["state"]
+    inner_dict_status = outer_dict["status"]
+    for item in arr:
+        if item['state'] == 'down' and item['status'] == 'disabled':
+              exit_state = STATE_OK
+              state_string = "OK"
+              print "Nova Compute status: {state_str}.".format(state_str=state_string)
+              sys.exit(exit_state)
+              return 0
+        
+        elif item['state'] == 'up' and item['status'] == 'disabled':
+              exit_state = STATE_WARNING
+              state_string = "WARNING"
+              print "Nova Compute status: {state_str}.".format(state_str=state_string)
+              sys.exit(exit_state)
+              return 1
+        
+        elif item['state'] == 'down' and item['status'] == 'enabled':
+              exit_state = STATE_CRITICAL
+              state_string = "CRITICAL"
+              print "Nova Compute status: {state_str}.".format(state_str=state_string)
+              sys.exit(exit_state)
+              return 2
+        
+        else:
+              exit_state = STATE_OK
+              state_string = "OK"
+    print "Nova Compute status: {state_str}.".format(state_str=state_string)
+    sys.exit(exit_state)
+
+
+getComputeState(getAuthToken())
